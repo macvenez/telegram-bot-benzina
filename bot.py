19a17976d28d5a15d5792be76324477cd6ccf02f
macvenez <macvenez@gmail.com>
2022-11-08 14:16:34 +0100

    added location handling

--- bot.py
+++ bot.py
@@ -1,2 +1,90 @@
-import token
-print(token.api_key)
+import _token
+
+apikey = _token.api_key
+
+import logging
+from telegram import (
+    Update,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import (
+    filters,
+    MessageHandler,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    InlineQueryHandler,
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Posizione", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+    print(update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = " ".join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(query.upper()),
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    current_pos = (update.message.location.latitude, update.message.location.longitude)
+    print(current_pos)
+
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(apikey).build()
+
+    start_handler = CommandHandler("start", start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler("caps", caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(MessageHandler(filters.LOCATION, location))
+
+    application.run_polling()
